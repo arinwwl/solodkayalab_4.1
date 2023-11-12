927a83a8fc0c03b9d0696b8cea4c89ad89608fa4
Arina <Arina@DESKTOP-H3T0JAQ>
2023-11-12 16:50:39 +0200

    Добавьте файлы проекта.

---
+++ solodkayalab_4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿Random massive = new Random();
+const int massiveSize = 30;
+int[] max = new int[massiveSize];
+for (int i = 0; i < max.Length; i++)
+{
+    max[i] = massive.Next(0, 100);
+    Console.Write(max[i] + " ");
+}
+Console.WriteLine();
+int count = 0;
+for (int i = 1; i < massiveSize - 1; ++i)
+{
+    if (max[i - 1] < max[i] && max[i + 1] < max[i])
+    {
+        Console.WriteLine($"{max[i - 1]} {max[i]} {max[i + 1]}");
+        count++;
+    }
+}
+Console.WriteLine(count);
+
